9461fb11e4e9a885f12ae5762f51991770b5791e
Markus <mr819@lindenwood.edu>
2026-03-31 13:44:48 -0500

    Main Menu fixes

--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -1,11 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 public class MainMenuScript : MonoBehaviour
 {
+    private Button playButton;
+    private Button quitButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnEnable()
     {
-        
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        playButton = root.Q<Button>("PlayButton");
+        quitButton = root.Q<Button>("QuitButton");
+    }
+
+    void OnPlayClicked()
+    {
+        Debug.Log("Play Clicked");
+    }
+
+    void OnQuitClicked()
+    {
+        Debug.Log("Quit Clicked");
     }
 
     // Update is called once per frame
