a8b1b025bda4bb0c314f78e6296959bf37e0ab97
Markus <mr819@lindenwood.edu>
2026-04-06 13:34:06 -0500

    Constant Code

---
+++ Assets/Scripts/GameConstants.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public static class GameConstants
+{
+    public const string MainMenuScene = "mainMenuScene";
+    public const string TaskScenario = "taskSenario";
+    public const string TaskResult = "taskResult";
+    public const string Wardrobe = "wardrobe";
+}
