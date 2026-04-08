63f0f94b2ceb4280768380324cbe03ee86a2a73d
Markus <mr819@lindenwood.edu>
2026-04-08 13:07:01 -0500

    Fixed String

--- Assets/Scripts/GameConstants.cs
+++ Assets/Scripts/GameConstants.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public static class GameConstants
 {
     public const string MainMenuScene = "mainMenuScene";
-    public const string TaskScenario = "taskSenario";
-    public const string TaskResult = "taskResult";
-    public const string Wardrobe = "wardrobe";
+    public const string TaskScenario = "taskScenarioScene";
+    public const string TaskResult = "taskResultScene";
+    public const string Wardrobe = "wardrobeScene";
 }
