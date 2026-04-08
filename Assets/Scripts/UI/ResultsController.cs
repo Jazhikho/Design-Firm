c9dbdfa0621578fe4d894335be4b9d7c8d81faed
TimboA88 <98147870+TimboA88@users.noreply.github.com>
2026-04-07 21:15:30 -0500

    Tuesday Commit 1: Temporary files for results screen, UI Functions, basic scoring.
    This commit implements a temporary scenario that allows for the results UI to display various values that would be in a real scenario. Such as a basic score mechanic.

--- Assets/Scripts/UI/ResultsController.cs
+++ Assets/Scripts/UI/ResultsController.cs
@@ -9,9 +9,18 @@ public class ResultsController : MonoBehaviour
     private Button btnMainMenu;
     private Button btnRetryScenario;
 
+    //
+    private VisualElement root;
+    public temporaryScenario tempScenario = new temporaryScenario();
+
+    public tempScenarioItemTesting currentScenario; //Change this to whatever Markus makes
+
+    public float totalScore;
+    //
+
     public void OnEnable()
     {
-        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        root = GetComponent<UIDocument>().rootVisualElement;
 
         btnRetry = root.Q<Button>("btnRetry");
         btnRetry.clicked += GoToWardrobe;
@@ -21,6 +30,14 @@ public class ResultsController : MonoBehaviour
 
         btnRetryScenario = root.Q<Button>("btnNewScenario");
         btnRetryScenario.clicked += NewScenario;
+
+        //RunSomeThings -ORGANIZE THIS
+            tempScenario.tempScenarioRun();
+            currentScenario = tempScenario.Scenario;
+            tempScenario.debugTempScenario(currentScenario);
+            readWardrobeItems();
+            readScenario();
+        //
     }
 
     public void OnDisable()
@@ -43,5 +60,79 @@ public class ResultsController : MonoBehaviour
     private void NewScenario()
     {
         SceneManager.LoadScene("taskScenarioScene"); // TODO: replace scene name with game constant
+    }
+
+    //Read wardrobe items
+    public void readWardrobeItems()
+    {
+        readItem("lblYourJacketName", "Jacket", wardrobeItemList.Instance.currentItemJacket);
+        readItem("lblYourTopName", "Top", wardrobeItemList.Instance.currentItemTop);
+        readItem("lblYourBottomsName", "Bottoms", wardrobeItemList.Instance.currentItemBottom);
+        readItem("lblYourShoesName", "Shoes", wardrobeItemList.Instance.currentItemShoe);
+    }
+    public void readItem(string slotLabel, string slotStr, wardrobeItemClothing itemSlot)
+    {
+        Label itemDisplay = root.Q<Label>(slotLabel);
+        itemDisplay.text = "Your " + slotStr + ": " + itemSlot.itemName;
+    }
+    //Read Scenario stuffs
+    public void readScenario()
+    {
+        scoring();
+        idealItemsHint();
+    }
+    public void idealItemsHint() //Clean this up
+    {
+        idealItemHint("lblHintIdealJacket", currentScenario.outfitGood.jacketIdeal.commentary);
+        idealItemHint("lblHintIdealTop", currentScenario.outfitGood.topIdeal.commentary);
+        idealItemHint("lblHintIdealBottoms", currentScenario.outfitGood.bottomsIdeal.commentary);
+        idealItemHint("lblHintIdealShoes", currentScenario.outfitGood.shoesIdeal.commentary);
+    }
+    public void idealItemHint(string label, string commentary)
+    {
+        Label idealHint;
+        idealHint = root.Q<Label>(label);
+        idealHint.text = "Hint: " + commentary;
+    }
+    public void feedBackItem(string label, string commentary)
+    {
+        Label idealHint;
+        idealHint = root.Q<Label>(label);
+
+        idealHint.text = commentary;
+    }
+    public void scoring()
+    {
+        scoreItem("lblJacketScore", wardrobeItemList.Instance.currentItemJacket.ID, "lblFeedbackJacket");
+        scoreItem("lblTopScore", wardrobeItemList.Instance.currentItemTop.ID, "lblFeedbackTop");
+        scoreItem("lblBottomsScore", wardrobeItemList.Instance.currentItemBottom.ID, "lblFeedbackBottoms");
+        scoreItem("lblShoesScore", wardrobeItemList.Instance.currentItemShoe.ID, "lblFeedbackShoes");
+
+        Label totalScoreDispaly;
+        totalScoreDispaly = root.Q<Label>("lblTotalScore");
+        totalScoreDispaly.text = totalScore.ToString() + "/4.0";
+    }
+    private void scoreItem(string label, string itemID, string commentaryLabel)
+    {
+        Label scoreDisplay;
+        scoreDisplay = root.Q<Label>(label);
+
+        bool anyFeedback = false;
+
+        foreach (scoredItem scoreItem in currentScenario.scoredItems)
+        {
+            if (scoreItem.itemID == itemID)
+            {
+                totalScore += scoreItem.score;
+                scoreDisplay.text = scoreItem.score.ToString();
+                
+                anyFeedback = true;
+                feedBackItem(commentaryLabel, scoreItem.commentary);
+            }
+        }
+        if (anyFeedback == false)
+        {
+            feedBackItem(commentaryLabel, "");
+        }
     }
 }
